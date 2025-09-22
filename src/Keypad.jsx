507052768d636653fc909f4b4814e428b16838ab
Likithkumarr <likithkumar385@gmail.com>
2025-09-22 18:52:10 +0530

    Add files via upload

---
+++ src/Keypad.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Keypad = () => {
+  return (
+    <div className='keypad'>
+      <div className='row'>
+        <button className='digit'>7</button>
+        <button className='digit'>8</button>
+        <button className='digit'>9</button>
+        <button className='operator'>/</button>
+      </div>
+    </div>
+  )
+}
+
+export default Keypad
